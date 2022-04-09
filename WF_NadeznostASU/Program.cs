f6062603381760f78e20fc51d04998090b941215
Axtra <axtra2@gmail.com>
2022-04-09 15:37:12 +0300

    Added high DPI hack

--- WF_NadeznostASU/Program.cs
+++ WF_NadeznostASU/Program.cs
@@ -2,12 +2,18 @@ namespace WF_NadeznostASU
 {
     internal static class Program
     {
+        // For high DPI hack
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        private static extern bool SetProcessDPIAware();
+
         /// <summary>
         ///  The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
+            if (Environment.OSVersion.Version.Major >= 6)
+                SetProcessDPIAware(); // High DPI hack
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
